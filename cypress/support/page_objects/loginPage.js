731659f6870f1c82f3a58cc336f14dd4c09aa8ae
Roman Mager <roman.mager@outlook.com>
2020-11-16 20:28:06 +0300

    Create page objects for login and main page

--- cypress/support/page_objects/loginPage.js
+++ cypress/support/page_objects/loginPage.js
@@ -1,6 +1,29 @@
+/// <reference types="cypress"/>
+
 class LoginPage {
 
+    /*
+    enterEmail() {
+        
+    }
 
+    enterPassword() {
+
+    }
+
+    submitForm() {
+
+    }
+    */
+
+    submitFormWithEmailAndPassword(email, password) {
+        cy.contains('.auth-container', 'Вход')
+            .find('form').then(form => {
+                cy.wrap(form).find('[placeholder="Ник или e-mail"]').type(email);
+                cy.wrap(form).find('[placeholder="Пароль"]').type(password);
+                cy.wrap(form).submit();
+            })
+    }
 }
 
 export default LoginPage;
--- cypress/support/page_objects/mainPage.js
+++ cypress/support/page_objects/mainPage.js
@@ -1,5 +1,18 @@
+/// <reference types="cypress"/>
+
 class MainPage {
 
+    navigate() {
+        cy.visit('/');
+    }
+
+    openLoginPage() {
+        cy.get('#userbar').then(userBar => {
+            if (cy.wrap(userBar).contains('Вход')) {
+                cy.wrap(userBar).click();
+            }
+        })
+    }
     
 }
 
